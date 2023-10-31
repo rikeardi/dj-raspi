58345d7a53c664ab7dec2602bfe0d9d44ee3c3a5
--set <--set>
2023-10-31 18:55:43 +0200

    ok

--- DJraspi/DJraspi/settings.py
+++ DJraspi/DJraspi/settings.py
@@ -11,12 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from sys import file
 import yaml
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 _configfile = BASE_DIR / "config/config.yml"
-config = yaml.safe_load(open(_configfile))
+config = yaml.safe_load_all(file(_configfile, "r"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
